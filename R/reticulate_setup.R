6b832a6a298e9f179084272ba96a338aced5a64f
marastadler <mara.stadler@live.de>
2022-03-28 17:55:10 +0200

    --

--- R/reticulate_setup.R
+++ R/reticulate_setup.R
@@ -2,22 +2,26 @@
 # https://rstudio.github.io/reticulate/articles/package.html
 
 # global reference to LassoNet
-lassonet <- NULL
+module <- NULL
 
 #.onLoad <- function(libname, pkgname) {
 #  # use superassignment to update global reference to scipy
-#  lassonet <<- reticulate::import("lassonet", delay_load = TRUE)
+#  module <<- reticulate::import('module')
 
 #}
 library(reticulate)
+
+
 install_lassonet <- function(method = "auto", conda = "auto") {
   # reticulate::py_install("lassonet", method = method, conda = conda, pip = TRUE)
   # vector of required python packages;
   #py_pack = c("numpy", "torch")
-  module = import_from_path("module", path = "/Users/mara.stadler/LRZ Sync+Share/PhD/lassonet_fab_daniele/",
-                   convert = TRUE, delay_load = FALSE)
+  #reticulate::py_install("module", "git+https://github.com/gnopuz83/lassonet@main", pip = T)
+  module = import_from_path("module", path = "/Users/mara.stadler/LRZ Sync+Share/PhD/lassonet_fab_daniele",
+                  convert = TRUE, delay_load = FALSE)
   
-  return(module)
-
+  #reticulate::py_install("git+https://github.com/gnopuz83/lassonet_fab@main", pip = T)
+  
 }
+
 module = install_lassonet()
